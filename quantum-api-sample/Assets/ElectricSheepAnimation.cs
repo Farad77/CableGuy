f15ddaa283db87c75e2c5b10faedeed4de28d654
gamayun974 <thierrybrochart@gmail.com>
2022-10-05 10:54:03 +0400

    wip mobs
    les mobs sont en wip

--- quantum-api-sample/Assets/ElectricSheepAnimation.cs
+++ quantum-api-sample/Assets/ElectricSheepAnimation.cs
@@ -5,29 +5,45 @@ using Photon.Deterministic;
 using UnityEngine;
 using Quantum;
 using UnityEngine.UI;
+using Spine.Unity;
+using Spine.Unity.AttachmentTools;
 
 public unsafe class ElectricSheepAnimation : MonoBehaviour
 {
     private EntityRef _entityRef = default;
 
     private Quaternion qInitRot;
+    private Transform tCameraTransform;
     private NavMeshSteeringAgent* _navMeshSteeringAgent = default;
+
+    [SerializeField] private Animator _animator = null;
+    private int TriggerWalk = Animator.StringToHash("WalkTrigger");
+    private int TriggerDeath = Animator.StringToHash("DeathTrigger");
+    private int TriggerHit = Animator.StringToHash("HitTrigger");
+    private int oldAnim;
 
     // This method is registered to the EntityView's OnEntityInstantiated event located on the parent GameObject
     public void Initialize(EntityRef entityRef)
     {
-        qInitRot = transform.rotation;
 
         _entityRef = entityRef;
 
         // Set up Quantum events
         QuantumEvent.Subscribe<EventOnDamageDealt>(this, TakeDamage);
 
+        oldAnim = TriggerWalk;
+        //qInitRot = transform.parent.rotation;
     }
     private void TakeDamage(EventOnDamageDealt obj)
     {
         if (obj.Target != _entityRef) return;
-        //_animator.SetTrigger(TRIGGER_DAMAGE_HIT);
+        _animator.SetTrigger(TriggerHit);
+    }
+
+    private void Start()
+    {
+        qInitRot = transform.rotation;
+        tCameraTransform = Camera.main.transform;
     }
 
     void LateUpdate()
@@ -43,18 +59,18 @@ public unsafe class ElectricSheepAnimation : MonoBehaviour
             //Debug.Log("look left");
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
-        transform.rotation = qInitRot; // THB dont rotate this child
+        //transform.parent.rotation = qInitRot; // THB dont rotate this child
+        transform.rotation = tCameraTransform.rotation * qInitRot;
     }
 
     public void Death()
     {
-        /*_animator.SetFloat(FLOAT_MOVEMENT_SPEED, 0.0f);
-        _animator.SetFloat(FLOAT_MOVEMENT_HORIZONTAL, 0.0f);
-        _animator.SetFloat(FLOAT_MOVEMENT_VERTICAL, 0.0f);
-
-        _animator.SetTrigger(TRIGGER_DEATH);*/
-
-        //Destroy(gameObject, _deathAnimationClip.length + 0.5f);
+        _animator.SetTrigger(TriggerDeath);
+        StartCoroutine(WaitB4Death());
+    }
+    public IEnumerator WaitB4Death()
+    {
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 }
